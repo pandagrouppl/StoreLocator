ecfb9438b137f3089d3cda1bd200319b9a9ba632
Tomasz Jezierski <tjezierski@light4website.com>
2017-02-27 11:38:58 +0100

    Final js

--- app/design/frontend/peterjacksons/petertheme/web/js/main.ts
+++ app/design/frontend/peterjacksons/petertheme/web/js/main.ts
@@ -1,16 +1,13 @@
 import * as Module1 from "./moduleone";
 import * as Module2 from "./moduletwo";
-import $ = require("jquery");
 
-export class Main {
+export = class Main {
 
     constructor() {
         console.log('elo');
-
     }
 
     start() {
-        let aasddssd: String = 'eeee';
         let m1 = new Module1.ModuleOne();
         let m2 = new Module2.ModuleTwo();
 
