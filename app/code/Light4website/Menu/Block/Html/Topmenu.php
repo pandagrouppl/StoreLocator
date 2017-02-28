e8e087e9f948c21dbe82e6ce383e17e25400550c
Tomasz Jezierski <tjezierski@light4website.com>
2017-02-28 16:03:44 +0100

    IP-52 New module

---
+++ app/code/Light4website/Menu/Block/Html/Topmenu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Light4website\Menu\Block\Html;
+
+class Topmenu extends \Magento\Theme\Block\Html\Topmenu
+{
+
+    protected function _addSubMenu($child, $childLevel, $childrenWrapClass, $limit)
+    {
+
+    }
+
+}
