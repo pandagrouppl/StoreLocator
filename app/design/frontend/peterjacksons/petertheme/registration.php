beb29e26b80df379c62e4aaa7092494ebab4a672
Adrian Kowalczewski <akowalczewski@light4website.com>
2017-03-01 16:29:06 +0100

    IP-52 inheritance introduced

--- app/design/frontend/peterjacksons/petertheme/registration.php
+++ app/design/frontend/peterjacksons/petertheme/registration.php
@@ -3,8 +3,4 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/peterjacksons/petertheme',
-    __DIR__
-);
+\Magento\Framework\Component\ComponentRegistrar::register(\Magento\Framework\Component\ComponentRegistrar::THEME,'frontend/peterjacksons/petertheme',__DIR__);
