c74208300d3df26479d8ad3d0afd36f7c45da56e
Tomasz Jezierski <tjezierski@light4website.com>
2017-03-06 09:40:09 +0100

    IP-52 Submenu module

--- app/code/Light4website/Menu/Block/Html/Topmenu.php
+++ app/code/Light4website/Menu/Block/Html/Topmenu.php
@@ -7,7 +7,23 @@ class Topmenu extends \Magento\Theme\Block\Html\Topmenu
 
     protected function _addSubMenu($child, $childLevel, $childrenWrapClass, $limit)
     {
+        $html = '';
+        if (!$child->hasChildren()) {
+            return $html;
+        }
 
+        $colStops = null;
+        if ($childLevel == 0 && $limit) {
+            $colStops = $this->_columnBrake($child->getChildren(), $limit);
+        }
+
+        $html .= '<ul class="level' . $childLevel . ' submenu">';
+        $html .= $this->_getHtml($child, $childrenWrapClass, $limit, $colStops);
+        $html .= '</ul>';
+
+        return $html;
     }
+
+
 
 }
