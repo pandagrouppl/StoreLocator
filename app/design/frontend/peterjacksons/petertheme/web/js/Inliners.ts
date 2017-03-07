006edebdb3f726db49430331d0b9628f8a09c031
Adrian Kowalczewski <akowalczewski@light4website.com>
2017-03-07 15:15:45 +0100

    IP-52 cart/search cleanup

--- app/design/frontend/peterjacksons/petertheme/web/js/Inliners.ts
+++ app/design/frontend/peterjacksons/petertheme/web/js/Inliners.ts
@@ -4,6 +4,7 @@ export class Inliners {
 
     constructor() {
         this._showSearch();
+        this._preventSpinnerClick();
     }
 
     private _showSearch(): void {
@@ -16,5 +17,11 @@ export class Inliners {
         $('.search-overlay__form').click((event) => {
             event.stopPropagation();
         });
+    }
+
+    private _preventSpinnerClick(): void {
+        $('.spinner').click((event) => {
+            event.preventDefault();
+        })
     }
  }
