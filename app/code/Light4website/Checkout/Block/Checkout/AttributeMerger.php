242887dfa75d91c69b26308c66b3ed9821d18443
Adrian Kowalczewski <akowalczewski@light4website.com>
2017-03-22 17:28:25 +0100

    IP-52 checkout

--- app/code/Light4website/Checkout/Block/Checkout/AttributeMerger.php
+++ app/code/Light4website/Checkout/Block/Checkout/AttributeMerger.php
@@ -11,6 +11,76 @@ use Magento\Customer\Api\CustomerRepositoryInterface as CustomerRepository;
 
 class AttributeMerger extends \Magento\Checkout\Block\Checkout\AttributeMerger
 {
+    protected function getFieldConfig(
+        $attributeCode,
+        array $attributeConfig,
+        array $additionalConfig,
+        $providerName,
+        $dataScopePrefix
+    ) {
+        // street attribute is unique in terms of configuration, so it has its own configuration builder
+        if (isset($attributeConfig['validation']['input_validation'])) {
+            $validationRule = $attributeConfig['validation']['input_validation'];
+            $attributeConfig['validation'][$this->inputValidationMap[$validationRule]] = true;
+            unset($attributeConfig['validation']['input_validation']);
+        }
+
+        if ($attributeConfig['formElement'] == 'multiline') {
+            return $this->getMultilineFieldConfig($attributeCode, $attributeConfig, $providerName, $dataScopePrefix);
+        }
+
+        $uiComponent = isset($this->formElementMap[$attributeConfig['formElement']])
+            ? $this->formElementMap[$attributeConfig['formElement']]
+            : 'Magento_Ui/js/form/element/abstract';
+        $elementTemplate = isset($this->templateMap[$attributeConfig['formElement']])
+            ? 'ui/form/element/' . $this->templateMap[$attributeConfig['formElement']]
+            : 'ui/form/element/' . $attributeConfig['formElement'];
+
+        $element = [
+            'component' => isset($additionalConfig['component']) ? $additionalConfig['component'] : $uiComponent,
+            'config' => [
+                // customScope is used to group elements within a single form (e.g. they can be validated separately)
+                'customScope' => $dataScopePrefix,
+                'customEntry' => isset($additionalConfig['config']['customEntry'])
+                    ? $additionalConfig['config']['customEntry']
+                    : null,
+                'template' => 'ui/form/field',
+                'elementTmpl' => isset($additionalConfig['config']['elementTmpl'])
+                    ? $additionalConfig['config']['elementTmpl']
+                    : $elementTemplate,
+                'tooltip' => isset($additionalConfig['config']['tooltip'])
+                    ? $additionalConfig['config']['tooltip']
+                    : null,
+            ],
+            'dataScope' => $dataScopePrefix . '.' . $attributeCode,
+            'label' => $attributeConfig['label'],
+            'placeholder' => isset($additionalConfig['placeholder'])
+                ? $additionalConfig['placeholder']
+                : $attributeConfig['label'] . '*',
+            'provider' => $providerName,
+            'sortOrder' => isset($additionalConfig['sortOrder'])
+                ? $additionalConfig['sortOrder']
+                : $attributeConfig['sortOrder'],
+            'validation' => $this->mergeConfigurationNode('validation', $additionalConfig, $attributeConfig),
+            'options' => $this->getFieldOptions($attributeCode, $attributeConfig),
+            'filterBy' => isset($additionalConfig['filterBy']) ? $additionalConfig['filterBy'] : null,
+            'customEntry' => isset($additionalConfig['customEntry']) ? $additionalConfig['customEntry'] : null,
+            'visible' => isset($additionalConfig['visible']) ? $additionalConfig['visible'] : true,
+        ];
+
+        if (isset($attributeConfig['value']) && $attributeConfig['value'] != null) {
+            $element['value'] = $attributeConfig['value'];
+        } elseif (isset($attributeConfig['default']) && $attributeConfig['default'] != null) {
+            $element['value'] = $attributeConfig['default'];
+        } else {
+            $defaultValue = $this->getDefaultValue($attributeCode);
+            if (null !== $defaultValue) {
+                $element['value'] = $defaultValue;
+            }
+        }
+        return $element;
+    }
+
     protected function getMultilineFieldConfig($attributeCode, array $attributeConfig, $providerName, $dataScopePrefix)
     {
         $lines = [];
@@ -33,14 +103,13 @@ class AttributeMerger extends \Magento\Checkout\Block\Checkout\AttributeMerger
                         $attributeConfig['validation']
                     )
                     : $attributeConfig['validation'],
-                'additionalClasses' => $isFirstLine ? : 'additional'
-
+                'additionalClasses' => $isFirstLine ? : 'additional',
+                'placeholder' => isset($additionalConfig['placeholder'])
+                    ? $additionalConfig['placeholder']
+                    : $attributeConfig['label'] . '*',
             ];
             if ($isFirstLine && isset($attributeConfig['default']) && $attributeConfig['default'] != null) {
                 $line['value'] = $attributeConfig['default'];
-            }
-            if($attributeCode=='street') {
-                $line['label'] = $attributeConfig['label'];
             }
             $lines[] = $line;
         }
@@ -54,7 +123,7 @@ class AttributeMerger extends \Magento\Checkout\Block\Checkout\AttributeMerger
             'type' => 'group',
             'config' => [
                 'template' => 'ui/group/group',
-                'additionalClasses' => $attributeCode
+                'additionalClasses' => $attributeCode,
             ],
             'children' => $lines,
         ];
