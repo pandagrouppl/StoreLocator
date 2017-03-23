cb60d12b2a51cf74f5037afae5a52c64a8b9e0b3
Tomasz Jezierski <tjezierski@light4website.com>
2017-03-23 11:07:43 +0100

    IP-52 Attribute module

---
+++ app/code/Light4website/Attributes/Setup/InstallData.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Light4website\Attributes\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallData implements InstallDataInterface
+{
+    /**
+     * EAV setup factory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Init
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if ($context->getVersion() && version_compare($context->getVersion(), '1.0.1') < 0) {
+
+            /** @var EavSetup $eavSetup */
+            $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+            /**
+             * Add attributes to the eav/attribute
+             */
+
+            $eavSetup->addAttribute(
+                \Magento\Catalog\Model\Product::ENTITY,
+                'hover_image',
+                [
+                    'type' => 'varchar',
+                    'backend' => '',
+                    'frontend' => '\Magento\Catalog\Model\Product\Attribute\Frontend\Image',
+                    'label' => 'Hover Image',
+                    'input' => 'media_image',
+                    'class' => '',
+                    'source' => '',
+                    'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                    'visible' => true,
+                    'required' => false,
+                    'user_defined' => false,
+                    'default' => 0,
+                    'searchable' => false,
+                    'filterable' => false,
+                    'comparable' => false,
+                    'visible_on_front' => false,
+                    'used_in_product_listing' => false,
+                    'unique' => false,
+                    'apply_to' => ''
+                ]
+            );
+        }
+
+        $setup->endSetup();
+    }
+}
