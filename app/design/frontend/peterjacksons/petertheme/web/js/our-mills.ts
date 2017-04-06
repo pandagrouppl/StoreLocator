fdbf5a5409b45847576316680670faf9a131b714
Adrian Kowalczewski <akowalczewski@light4website.com>
2017-04-06 13:03:13 +0200

    ip-52 our mills youtube

--- app/design/frontend/peterjacksons/petertheme/web/js/our-mills.ts
+++ app/design/frontend/peterjacksons/petertheme/web/js/our-mills.ts
@@ -9,5 +9,59 @@ export class OurMills {
 
     _youtubeOverlay(): void {
 
+        const ovrl = 'youtube-player-overlay';
+
+        $('.youtube-player__play-img').forEach((item, index) => {
+            console.log(item, ' AND ' ,index)
+        });
+
+        $('.youtube-player__play-img')[0].bind('click', () => {
+            $(`.${ovrl}`).addClass(`${ovrl}--active`);
+            $('#frame-movie-2')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+        });
+        //.forEach(() => {
+        //
+        //    }
+        //
+        //);
+
+        $('.popup-movie-1').on('click', function () {
+            $('.popup-overlay-movie-1').addClass('active');
+            $('#frame-movie-1')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+        });
+
+        $('body').on('click touchstart','.popup-overlay.active', function () {
+            $(this).removeClass('active');
+            if( $('body').hasClass('cms-our-mills')) {
+                $('.video-container iframe')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+                $('.video-container iframe')[1].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+                $('.video-container iframe')[2].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+            }
+        });
+
+
+
+        $('.popup-movie-2').on('click', function () {
+            $('.popup-overlay-movie-2').addClass('active');
+            $('#frame-movie-2')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+        });
+
+        $('.popup-movie-3').on('click', function () {
+            $('.popup-overlay-movie-3').addClass('active');
+            $('#frame-movie-3')[0].contentWindow.postMessage('{"event":"command","func":"playVideo","args":""}', '*');
+        });
+
+        $('.popup-movie-close-1').on('click', function () {
+            $('#frame-movie-1')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+        });
+
+        $('.popup-movie-close-2').on('click', function () {
+            $('#frame-movie-2')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+        });
+
+        $('.popup-movie-close-3').on('click', function () {
+            $('#frame-movie-3')[0].contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+        });
+        
     }
 }
