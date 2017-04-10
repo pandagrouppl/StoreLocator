18db0340f9bd89c16eb918315076fe36a95ccb35
Adrian Kowalczewski <akowalczewski@light4website.com>
2017-04-10 10:35:17 +0200

    ip-52 hoursselectfill changed to stateless component

--- view/frontend/preact/src/component/HoursSelectFill.js
+++ view/frontend/preact/src/component/HoursSelectFill.js
@@ -1,27 +1,23 @@
-import { h, Component} from 'preact';
+import { h } from 'preact';
 
-export default class HoursSelectFill extends Component {
-    constructor() {
-        super();
-    }
-
-    closedCheck(hours_arr) {
+const HoursSelectFill = (props) => {
+    const closedCheck(hours_arr) => {
         if (hours_arr[0].toLowerCase() == 'closed') {
             return hours_arr[0];
         } else {
             return hours_arr.join(' - ');
         }
-    }
+    };
 
-    render () {
-        return (
-            <div class="stores-li__hours-wrapper">
-                <select class="stores-li__hours">
-                    <option>STORE HOURS</option>
-            {Object.keys(this.props.day).map((days) => <option>{days} : {this.closedCheck(this.props.day[days])}</option>)}
-                </select>
-                <figure class="stores-li__hours-arrow"></figure>
-            </div>
-        )
-    }
-}
+    return (
+        <div class="stores-li__hours-wrapper">
+            <select class="stores-li__hours">
+                <option>STORE HOURS</option>
+                {Object.keys(props.day).map((days) => <option>{days} : {closedCheck(props.day[days])}</option>)}
+            </select>
+            <figure class="stores-li__hours-arrow"></figure>
+        </div>
+    )
+};
+
+export default HoursSelectFill;
