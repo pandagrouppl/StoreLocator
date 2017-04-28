3e240ac17c973bdc7a484a84aa121815e485f956
Adrian Kowalczewski <akowalczewski@light4website.com>
2017-04-28 11:22:01 +0200

    ip-52 map working

--- view/frontend/preact/src/container/HeaderMap.js
+++ view/frontend/preact/src/container/HeaderMap.js
@@ -1,6 +1,6 @@
 import { h, Component } from 'preact';
 import { connect } from 'mobx-preact';
-import Map from 'google-maps-react';
+import Map from '../component/Map';
 
 import Directions from './Directions';
 import Marker from '../component/Marker';
@@ -10,12 +10,13 @@ export default class HeaderMap extends Component {
 
     render() {
         return(
-            <section  style={{width: '100%', height: '400px', position: 'relative', overflow: 'hidden'}}>
+            <section style={{width: '100%', height: '400px', position: 'relative', overflow: 'hidden'}}>
             <Map className='google-maps-container'
+                 style={{width: '100%', height: '400px', position: 'relative', color:'pink'}}
                  google={this.props.google}
                  zoom={this.props.stateStore.zoom}
-                 style={{width: '100%', height: '400px', position: 'relative'}}
                  initialCenter={this.props.stateStore.geoTotal}
+                 scrollwheel={false}
                  center={this.props.stateStore.geoTotal}>
                     {this.props.stateStore.stores.map((store) => (
                         <Marker key={store.name} position={{lat: store.geo.lat, lng: store.geo.lng}} />
