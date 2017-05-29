e3c235028095b002362de5f9058621ab6dc53f9d
Karol Brzozowski <kbrzozowski@light4website.com>
2017-05-29 09:29:00 +0200

    [PJ-617] Created installer and uninstaller

---
+++ Setup/Uninstall.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace PandaGroup\StoreLocator\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UninstallInterface;
+
+class Uninstall implements UninstallInterface
+{
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        /**
+         * Delete table 'storelocator_image'
+         */
+        $setup->getConnection()->dropTable('storelocator_image');
+
+        /**
+         * Delete table 'storelocator_value'
+         */
+        $setup->getConnection()->dropTable('storelocator_value');
+
+        /**
+         * Delete table 'storelocator_tag'
+         */
+        $setup->getConnection()->dropTable('storelocator_tag');
+
+        /**
+         * Delete table 'storelocator_specialday'
+         */
+        $setup->getConnection()->dropTable('storelocator_specialday');
+
+        /**
+         * Delete table 'storelocator_holiday'
+         */
+        $setup->getConnection()->dropTable('storelocator_holiday');
+
+        /**
+         * Delete table 'storelocator'
+         */
+        $setup->getConnection()->dropTable('storelocator');
+
+        $setup->endSetup();
+    }
+}
