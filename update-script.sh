15fddf545cda1f1f94a82be9ad4c45ee28c25489
rataj <kratajczyk@light4website.com>
2017-05-30 17:33:43 +0200

    [hotfix]Add https pushurl to storelocator module

--- update-script.sh
+++ update-script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Update Peterjacksons script
 
+cd app/code/PandaGroup/StoreLocator && git remote set-url origin https://github.com/pandagrouppl/storelocator --push && cd ../../../..
 bin/magento setup:upgrade && bin/magento setup:di:compile && bin/magento setup:static-content:deploy
 cd tools && npm install && gulp compile && cd ..
 cd app/code/PandaGroup/StoreLocator/view/frontend/preact && npm install && npm run server && cd ../../../../../../..
+
 
