b708dd1c26ccc378ddacfacd25e9672afbcea94a
Tomasz Jezierski <tjezierski@light4website.com>
2017-06-08 11:16:04 +0200

    IP-52 Change owner script

--- update-script.sh
+++ update-script.sh
@@ -2,10 +2,13 @@
 # Update Peterjacksons script
 
 php bin/magento maintenance:enable
+OWNER=$(stat -c '%U' index.php)
+GROUP=$(stat -c '%G' index.php)
 cd app/code/PandaGroup/StoreLocator && git remote set-url origin https://github.com/pandagrouppl/storelocator --push && cd ../../../..
 php bin/magento setup:upgrade && php bin/magento setup:di:compile && php bin/magento setup:static-content:deploy && php bin/magento cache:flush
 cd tools && npm install && gulp compile && cd ..
 cd app/code/PandaGroup/StoreLocator/view/frontend/preact && npm install && npm run server && cd ../../../../../../..
+chown -R $OWNER:$GROUP .
 php bin/magento maintenance:disable
 
 
