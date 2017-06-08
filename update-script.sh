d05f36a6819da6d8b913bb673f1ef0ba12cee25a
Adrian Kowalczewski <adrian.kowalczewski@gmail.com>
2017-06-08 11:26:59 +0200

    PJ-646 two-step checkout finished

--- update-script.sh
+++ update-script.sh
@@ -2,10 +2,19 @@
 # Update Peterjacksons script
 
 php bin/magento maintenance:enable
-cd app/code/PandaGroup/StoreLocator && git remote set-url origin https://github.com/pandagrouppl/storelocator --push && cd ../../../..
-php bin/magento setup:upgrade && php bin/magento setup:di:compile && php bin/magento setup:static-content:deploy && php bin/magento cache:flush
-cd tools && npm install && gulp compile && cd ..
-cd app/code/PandaGroup/StoreLocator/view/frontend/preact && npm install && npm run server && cd ../../../../../../..
+cd app/code/PandaGroup/StoreLocator
+git remote set-url origin https://github.com/pandagrouppl/storelocator --push
+cd ../../../..
+php bin/magento setup:upgrade
+php bin/magento setup:di:compile
+php bin/magento setup:static-content:deploy
+php bin/magento cache:flush
+cd tools
+npm install
+gulp compile
+cd ..
+cd app/code/PandaGroup/StoreLocator/view/frontend/preact
+npm install
+npm run server
+cd ../../../../../../..
 php bin/magento maintenance:disable
-
-
