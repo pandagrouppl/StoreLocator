4836d42eaca096400a8f1daa53d31b0e45cb9cda
Tomasz Jezierski <tjezierski@light4website.com>
2017-06-08 10:31:17 +0200

    IP-52 Script root detect fix

--- install-script.sh
+++ install-script.sh
@@ -6,7 +6,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-if [[ $EUID > 0 ]]; then
+if [[ $EUID = 0 ]]; then
     nvm install node
     npm install -g gulp-cli
 else
