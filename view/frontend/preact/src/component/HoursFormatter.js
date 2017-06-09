648d04d1ea7124211e1c58a3d3371c821bc5e847
Adrian Kowalczewski <adrian.kowalczewski@gmail.com>
2017-06-09 15:55:04 +0200

    PJ-649 closed fix

--- view/frontend/preact/src/component/HoursFormatter.js
+++ view/frontend/preact/src/component/HoursFormatter.js
@@ -2,7 +2,7 @@ import { h } from 'preact';
 
 const HoursFormatter = (props) => {
     const closedCheck = (hours_arr) => {
-        return (hours_arr[0].toLowerCase() == 'closed') ? hours_arr[0] : hours_arr.join(' - ');
+        return (hours_arr[0].toLowerCase() == '0:0') ? 'closed' : hours_arr.join(' - ');
     };
 
     const Wrapper = props.wrapper;
