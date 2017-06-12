4fd2c3ab4b2b1e60e0ee10ae6d734a04f381c41e
Adrian Kowalczewski <adrian.kowalczewski@gmail.com>
2017-06-12 14:51:36 +0200

    PJ-649 added proper regions geoloc 2

--- Model/StoreLocator.php
+++ Model/StoreLocator.php
@@ -20,8 +20,8 @@ class StoreLocator extends AbstractModel
 
         $constants = [
             'apiKey'    => 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo',
-            'geo'       => ['lat' => 52.4046, 'lng' => 16.9252],
-            'zoom'      => 7,
+            'geo'       => [ 'lat' => -31.2532183, 'lng' => 146.921099 ],
+            'zoom'      => 9,
             'country'   => 'au',
             'pin'       => 'http://www.peterjacksons.com/media/storelocator/images/icon/pinpj2.png'
         ];
@@ -30,22 +30,22 @@ class StoreLocator extends AbstractModel
             [
                 'name'  => 'VIC',
                 'geo'   => ['lat' => -37.4713077, 'lng' => 144.7851531],
-                'zoom'  => 9
+                'zoom'  => 5
             ],
             [
                 'name' => 'SA',
                 'geo' => [ 'lat' => -30.0002315, 'lng' => 136.2091547 ],
-                'zoom' => 9
+                'zoom' => 5
             ],
             [
                 'name' => 'QLD',
                 'geo' => [ 'lat' => -20.9175738, 'lng' => 142.7027956 ],
-                'zoom' => 9
+                'zoom' => 5
             ],
             [
                 'name' => 'NSW',
                 'geo' => [ 'lat' => -31.2532183, 'lng' => 146.921099 ],
-                'zoom' => 9
+                'zoom' => 5
             ],
             [
                 'name' => 'ACT',
