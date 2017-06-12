9b119af8944e90f87428c1c17b9804a0fadd175a
Adrian Kowalczewski <adrian.kowalczewski@gmail.com>
2017-06-12 14:48:21 +0200

    PJ-649 added proper regions geoloc

--- Model/StoreLocator.php
+++ Model/StoreLocator.php
@@ -21,21 +21,36 @@ class StoreLocator extends AbstractModel
         $constants = [
             'apiKey'    => 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo',
             'geo'       => ['lat' => 52.4046, 'lng' => 16.9252],
-            'zoom'      => 11,
+            'zoom'      => 7,
             'country'   => 'au',
-            'pin'       => 'http://i.imgur.com/cOHpOCp.png'
+            'pin'       => 'http://www.peterjacksons.com/media/storelocator/images/icon/pinpj2.png'
         ];
 
         $regions = [
             [
-                'name'  => 'WLKP',
-                'geo'   => ['lat' => 52.360719, 'lng' => 17.259426],
+                'name'  => 'VIC',
+                'geo'   => ['lat' => -37.4713077, 'lng' => 144.7851531],
                 'zoom'  => 9
             ],
             [
-                'name'  => 'LUB',
-                'geo'   => ['lat' => 52.170800, 'lng' => 15.254622],
-                'zoom'  => 8
+                'name' => 'SA',
+                'geo' => [ 'lat' => -30.0002315, 'lng' => 136.2091547 ],
+                'zoom' => 9
+            ],
+            [
+                'name' => 'QLD',
+                'geo' => [ 'lat' => -20.9175738, 'lng' => 142.7027956 ],
+                'zoom' => 9
+            ],
+            [
+                'name' => 'NSW',
+                'geo' => [ 'lat' => -31.2532183, 'lng' => 146.921099 ],
+                'zoom' => 9
+            ],
+            [
+                'name' => 'ACT',
+                'geo' => [ 'lat' => -35.4734679, 'lng' => 149.0123679 ],
+                'zoom' => 9
             ]
         ];
 
