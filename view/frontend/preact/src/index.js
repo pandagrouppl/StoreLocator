4f88898fbbe5bd60b82f51c024b6b714639f02cd
Tomasz Jezierski <tjezierski@light4website.com>
2017-06-13 14:44:05 +0200

    PJ-649 Addded loder

--- view/frontend/preact/src/index.js
+++ view/frontend/preact/src/index.js
@@ -2,11 +2,29 @@ import { h, render } from 'preact';
 import App from './App';
 import { useStrict } from 'mobx';
 
-export function init(json) {
+
+const dots = (loader) => {
+    let i = 0;
+    setInterval(function() {
+        i++;
+        loader.insertAdjacentHTML('beforeend', '<span>.</span>');
+        if (i >= 4) {
+            i = 0;
+            loader.querySelectorAll("span").forEach(span => {
+                span.remove();
+            });
+        }
+    }, 500);
+};
+
+export function init() {
     useStrict(true);
+    const loader = document.getElementById('preact-loader');
+    dots(loader);
     fetch('/storelocator/index/json')
         .then(data => data.json())
         .then(json => {
+            loader.remove();
             render(<App json={json}/>, document.getElementById('preact-root'));
         });
 }
