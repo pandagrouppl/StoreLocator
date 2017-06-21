0ec4e35452e3beab1d71636b7b07120b3695a17d
Adrian Kowalczewski <adrian.kowalczewski@gmail.com>
2017-06-21 15:02:43 +0200

    PJ-638 script improvement

--- update-script.sh
+++ update-script.sh
@@ -4,22 +4,13 @@
 php bin/magento maintenance:enable
 OWNER=$(stat -c '%U' index.php)
 GROUP=$(stat -c '%G' index.php)
-cd app/code/PandaGroup/StoreLocator
-git remote set-url origin https://github.com/pandagrouppl/storelocator --push
-cd ../../../..
+(cd app/code/PandaGroup/StoreLocator && git remote set-url origin https://github.com/pandagrouppl/storelocator --push)
 php bin/magento setup:upgrade
 php bin/magento setup:di:compile
 php bin/magento setup:static-content:deploy
 php bin/magento cache:flush
-cd tools
-npm install
-gulp compile
-cd ..
-cd app/code/PandaGroup/StoreLocator/view/frontend/preact
-npm install
-npm run server
-cd ../../../../../../..
+(cd tools && npm install && gulp compile)
+(cd app/code/PandaGroup/StoreLocator/view/frontend/preact && npm install && npm run server)
+(cd wp/wp-content/themes/fishpig/ && chmod -R 775 .)
 chown -R $OWNER:$GROUP .
 php bin/magento maintenance:disable
-
-
