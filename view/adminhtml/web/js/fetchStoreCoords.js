63dca111212a320979ca91bf8cea1282244c3156
Adrian Kowalczewski <adrian.kowalczewski@gmail.com>
2017-07-12 15:02:09 +0200

    PJ-617 fetchStoreCoords - introduced google API formated address message, refactoring

--- view/adminhtml/web/js/fetchStoreCoords.js
+++ view/adminhtml/web/js/fetchStoreCoords.js
@@ -5,32 +5,35 @@ define(['jquery','Magento_Ui/js/form/components/button'], function ($, Button) {
         action: function () {
             var $lat = $('.admin__control-text[name="latitude"]');
             var $lng = $('.admin__control-text[name="longtitude"]');
-            var $zoom = $('.admin__control-select[name="zoom_level"]');
             $lat.css({transition: "border 1s"});
             $lng.css({transition: "border 1s"});
+            var $zoom = $('.admin__control-select[name="zoom_level"]');
             var $cntry = $('.admin__control-select[name="country"]');
+            var $cty = $('.admin__control-text[name="city"]');
             var $addr = $('.admin__control-text[name="address"]');
+            var $message = $('#google-api-message');
+            if (!$message.length) {
+                $('button[data-index="google-coords-fetch"]').after('<span id="google-api-message"></span>');
+                $message = $($message.selector);
+            }
             if ($zoom.val() === " ") {
                 $zoom.val("15").trigger("change");
             }
-            var err_g = 'Google Api Error! Cannot find such place! Check your Address!';
-            var err_ajax = 'Internal Error! Check your internet connection!';
             $.ajax({
                 url: 'http://maps.google.com/maps/api/geocode/json',
-                data: {address: $cntry.val() + ' ' + $addr.val()},
+                context: this,
+                data: {address: $cntry.val() + ' ' + $cty.val() + ' ' + $addr.val()},
                 method: 'get'
             }).done(function(json) {
                 if (json.status == 'OK') {
+                    $message.text(' Successfully fetched coordinates for address: ' + json.results[0].formatted_address);
                     $lat.val(json.results[0].geometry.location.lat).css({border: "1px solid #10bf10"}).trigger("change");
                     $lng.val(json.results[0].geometry.location.lng).css({border: "1px solid #10bf10"}).trigger("change");
                 } else {
-                    $lat.val(err_g).css({border: "1px solid #ff0050"});
-                    $lng.val(err_g).css({border: "1px solid #ff0050"})
+                    $message.text(' Google Api Error! Cannot find such place! Check your Address!');
                 }
             }).fail(function() {
-                window.alert(err_ajax);
-                $lat.val(err_ajax).css({border: "1px solid #ff0050"});
-                $lng.val(err_ajax).css({border: "1px solid #ff0050"})
+                $message.text(' Internal Error! Check your internet connection!');
             });
         }
     });
