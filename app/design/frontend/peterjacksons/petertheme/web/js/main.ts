c4452df598f11e4bf1c8502f546b6e72f4876050
Adrian Kowalczewski <adrian.kowalczewski@gmail.com>
2017-08-25 13:24:21 +0200

    PJ-635 ourmills JS init moved to template

--- app/design/frontend/peterjacksons/petertheme/web/js/main.ts
+++ app/design/frontend/peterjacksons/petertheme/web/js/main.ts
@@ -4,7 +4,6 @@ import ko = require("knockout");
 import * as SizeChart from "./SizeChart";
 import * as Inliners from "./Inliners";
 import * as newsletter from "./newsletter";
-import * as mills from "./our-mills";
 import * as popups from "./popups";
 
 export = class Main {
@@ -14,7 +13,6 @@ export = class Main {
 
     start() {
 
-        const ourMills = new mills.OurMills();
         $(() =>  {
             const inliners = new Inliners.Inliners();
             const newsletter1 = newsletter('subscribe-form-footer');
