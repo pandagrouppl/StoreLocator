7af5bc51f86cc2ad2f2d09540bb3158085bd251d
Adrian <akowalczewski@light4website.com>
2017-11-09 17:18:11 +0100

    PJ-667 configurable and minicart fix

--- app/design/frontend/peterjacksons/petertheme/requirejs-config.js
+++ app/design/frontend/peterjacksons/petertheme/requirejs-config.js
@@ -8,9 +8,8 @@ var config = {
     },
     "map": {
         "*": {
-            "catalogAddToCart": "js/extend/catalog-add-to-cart"
-            //'Magento_Ui/js/form/element/abstract': 'Vendor_ModuleName/js/form/components/collection',
-            //'Magento_Ui/templates/form/element/input.html': 'Vendor_ModuleName/template/form/components/collection.html'
+            "catalogAddToCart": "js/extend/catalog-add-to-cart",
+            "configurable": "js/extend/configurable-extend"
         }
     }
 };
