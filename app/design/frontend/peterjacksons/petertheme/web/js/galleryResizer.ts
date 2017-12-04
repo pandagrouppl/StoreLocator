ec54b408bd2ba95f27a748f175541fca1227420f
Adrian <adrian@adyry.com>
2017-12-04 12:29:40 +0100

    PJ-667 pdp gallery resizing fix

--- app/design/frontend/peterjacksons/petertheme/web/js/galleryResizer.ts
+++ app/design/frontend/peterjacksons/petertheme/web/js/galleryResizer.ts
@@ -10,8 +10,7 @@ const module = (config, element) => {
         const $window = $(window);
         const $stage = $('.fotorama__stage');
         const resize = () => {
-            console.log(!($element.hasClass('fotorama--fullscreen')));
-            if ($window.width() > 768) {
+            if ($window.width() > 768 && !(window.frameElement) && !(window.frameElement.nodeName === "IFRAME")) {
                 $stage.css({'max-height': '58vh'});
             } else {
                 $stage.css({'max-height': 'none'});
