ccf11606b320754d8f96dd6d09ebd008ece17a19
Adrian <adrian@adyry.com>
2017-12-14 20:19:54 +0100

    debounce added on gallery resizer

--- app/design/frontend/peterjacksons/petertheme/web/js/galleryResizer.ts
+++ app/design/frontend/peterjacksons/petertheme/web/js/galleryResizer.ts
@@ -1,7 +1,8 @@
 import $ = require("jquery");
+import _ = require('underscore');
 
 /**
- *
+ * Resizes gallery to "fit under fold" as Jade required
  */
 
 const module = (config, element) => {
@@ -9,13 +10,14 @@ const module = (config, element) => {
     $element.on('gallery:loaded', () => {
         const $window = $(window);
         const $stage = $('.fotorama__stage');
-        const resize = () => {
+        const resize = _.debounce(() => {
+            console.log('resize');
             if ($window.width() > 768 && !(window.frameElement)) {
                 $stage.css({'max-height': '58vh'});
             } else {
                 $stage.css({'max-height': 'none'});
             }
-        };
+        }, 250);
         resize();
         $window.resize((e) => resize());
     });
