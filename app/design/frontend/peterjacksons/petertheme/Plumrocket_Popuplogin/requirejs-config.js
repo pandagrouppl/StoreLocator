bdc0db3fc67a34c23be32dc23cf458842b3205a7
Axel Stachowski <astachowski@light4website.com>
2018-01-16 16:04:16 +0100

    Account popup login, homepage z index, contact button change, corporate pixel styling

---
+++ app/design/frontend/peterjacksons/petertheme/Plumrocket_Popuplogin/requirejs-config.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    "map": {
+        "*": {
+            regionUpdater: 'Magento_Checkout/js/region-updater',
+            "Plumrocket_Popuplogin/web/template/modal.html":
+                "PandaGroup_PopuploginExtender/web/template/modal.html",
+            "Plumrocket_Popuplogin/web/template/themes/lightSilver/forms/login.html":
+                "PandaGroup_PopuploginExtender/web/template/themes/lightSilver/forms/login.html",
+            "Plumrocket_Popuplogin/web/template/themes/lightSilver/forms/registration.html":
+                "PandaGroup_PopuploginExtender/web/template/themes/lightSilver/forms/registration.html"
+        },
+
+    }
+};
