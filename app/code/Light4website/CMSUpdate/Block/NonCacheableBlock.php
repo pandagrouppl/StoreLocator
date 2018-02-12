d1f028b6d2a9ccafde10aed511235f373ed06f7f
Axel Stachowski <astachowski@light4website.com>
2018-02-12 15:17:24 +0100

    mobile fixes

--- app/code/Light4website/CMSUpdate/Block/NonCacheableBlock.php
+++ app/code/Light4website/CMSUpdate/Block/NonCacheableBlock.php
@@ -1,7 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: developer
- * Date: 09.02.18
- * Time: 12:54
- */
+
+namespace Light4website\CMSUpdate\Block;
+
+class NonCacheableBlock extends \Magento\Cms\Block\Block
+{
+    /**
+     * NonCacheableBlock constructor.
+     *
+     * @param \Magento\Framework\View\Element\Context $context
+     * @param \Magento\Cms\Model\Template\FilterProvider $filterProvider
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Cms\Model\BlockFactory $blockFactory
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Context $context,
+        \Magento\Cms\Model\Template\FilterProvider $filterProvider,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Cms\Model\BlockFactory $blockFactory,
+        array $data = [])
+    {
+        parent::__construct($context, $filterProvider, $storeManager, $blockFactory, $data);
+    }
+
+    public function getCacheLifetime()
+    {
+        return null;
+    }
+
+    public function getCacheKeyInfo()
+    {
+        return [];
+    }
+}
