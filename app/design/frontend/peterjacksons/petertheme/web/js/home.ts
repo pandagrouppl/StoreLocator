b88f5c06cded4f07860de7a14b552706e1435bf3
Axel Stachowski <astachowski@light4website.com>
2018-02-15 16:11:36 +0100

    new homepage update

--- app/design/frontend/peterjacksons/petertheme/web/js/home.ts
+++ app/design/frontend/peterjacksons/petertheme/web/js/home.ts
@@ -4,6 +4,7 @@ import "slick";
 const home = () => {
 
     const $slider = $( '.slick-init' );
+    const $widget = $('.widget__slider-container');
 
     $slider.slick({
         infinite: true,
@@ -14,6 +15,45 @@ const home = () => {
         speed: 2500,
         prevArrow: '<div class="slick-prev"></div>',
         nextArrow: '<div class="slick-next"></div>',
+    });
+
+    $widget.slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<div class="arrows prev"></div>',
+        nextArrow: '<div class="arrows next"></div>',
+        responsive: [
+            {
+                breakpoint: 1260,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 375,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
     $( '.main-banner__scroll-down' ).click(function(evt) {
