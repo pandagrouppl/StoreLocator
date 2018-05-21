859149368fd19b3d926552ae450f40d556da72b8
Anna Linka <azlinka@light4website.com>
2018-05-21 15:25:59 +0200

    PJ-886 Add product name in success popup Add to cart, plus correct files php code

--- app/design/frontend/peterjacksons/petertheme/web/js/success-popup.ts
+++ app/design/frontend/peterjacksons/petertheme/web/js/success-popup.ts
@@ -1,16 +1,16 @@
-import $ = require("jquery");
-
-/**
- * Inserts image to success popup
- */
-
-const module = (config, element) => {
-    const $element = $(element);
-    $('[data-gallery-role="gallery-placeholder"]').on('fotorama:load', () => {
-        $element.attr('src', $('.fotorama__img')[0].src);
-    });
-
-
-};
-
-export = module;
+// import $ = require("jquery");
+//
+// /**
+//  * Inserts image to success popup
+//  */
+//
+// const module = (config, element) => {
+//     const $element = $(element);
+//     $('[data-gallery-role="gallery-placeholder"]').on('fotorama:load', () => {
+//         $element.attr('src', $('.fotorama__img')[0].src);
+//     });
+//
+//
+// };
+//
+// export = module;
