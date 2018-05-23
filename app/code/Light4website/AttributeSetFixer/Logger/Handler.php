ab14e250a15093dcf3ed525d1adb5ea11479c1c9
root <root@mgt-dev-70>
2018-05-23 11:31:39 +0000

    First version of automate

---
+++ app/code/Light4website/AttributeSetFixer/Logger/Handler.php
@@ -0,0 +1,19 @@
+<?php
+namespace Light4website\AttributeSetFixer\Logger;
+
+use \Monolog\Logger;
+
+class Handler extends \Magento\Framework\Logger\Handler\Base
+{
+    /**
+     * Logging level
+     * @var int
+     */
+    protected $loggerType = Logger::INFO;
+
+    /**
+     * File name
+     * @var string
+     */
+    protected $fileName = '/var/log/pandagroup_attributesetfixer.log';
+}
