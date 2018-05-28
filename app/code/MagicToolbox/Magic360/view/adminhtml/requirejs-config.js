7d50ea11fb286b019fadbce3b00788aaddbb3fe7
Karol Brzozowski <brzozowski96@gmail.com>
2018-05-28 16:59:04 +0200

    Moved third-party modules to panda repositories

--- app/code/MagicToolbox/Magic360/view/adminhtml/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-
-var config = {
-    map: {
-        '*': {
-            magic360:        'MagicToolbox_Magic360/js/script',
-            magic360Gallery: 'MagicToolbox_Magic360/js/magic360-gallery'
-        }
-    }
-};
