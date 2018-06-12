ed50a32cc9d0071036ffc39f986c7d308371d3a0
root <root@mgt-development-environment-70>
2018-06-12 11:36:42 +0000

    PJ-912 Slick slider adjustements on homepage

--- app/design/frontend/peterjacksons/petertheme/web/js/home.ts
+++ app/design/frontend/peterjacksons/petertheme/web/js/home.ts
@@ -20,43 +20,42 @@ const home = () => {
     $widget.slick({
         infinite: true,
         slidesToShow: 4,
-        slidesToScroll: 2,
+        // slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2500,
-        speed: 1500,
-        arrows: true,
-        prevArrow: '<div class="arrows prev"></div>',
-        nextArrow: '<div class="arrows next"></div>',
+        autoplaySpeed: 0,
+        speed: 5000,
+        arrows: false,
+        cssEase: 'linear',
+        dots: true,
+        centerMode: true,
         responsive: [
             {
                 breakpoint: 1260,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 2
+                    slidesToScroll: 1
+
                 }
             },
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 2,
-                    swipeToSlide: true
+                    slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 750,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2,
-                    swipeToSlide: true
+                    slidesToScroll: 1
                 }
             },
             {
-                breakpoint: 415,
+                breakpoint: 375,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1,
-                    swipeToSlide: true
+                    slidesToScroll: 1
                 }
             }
         ]
