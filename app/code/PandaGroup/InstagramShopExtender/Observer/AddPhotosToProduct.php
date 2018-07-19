da7b304481634b8bc0aed2966faee3f9e5e7d819
Karol Brzozowski <brzozowski96@gmail.com>
2018-07-19 17:42:52 +0200

    Add delete possibility to instagram tags

--- app/code/PandaGroup/InstagramShopExtender/Observer/AddPhotosToProduct.php
+++ app/code/PandaGroup/InstagramShopExtender/Observer/AddPhotosToProduct.php
@@ -2,11 +2,9 @@
 
 namespace PandaGroup\InstagramShopExtender\Observer;
 
-use Magenest\InstagramShop\Model\PhotoFactory;
 use Magenest\InstagramShop\Ui\DataProvider\Product\Form\Modifier\InstagramPhotos;
 use Magento\Catalog\Model\Product;
 use Magento\Framework\App\ObjectManager;
-use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Exception\CouldNotSaveException;
@@ -15,5 +13,66 @@ use Psr\Log\LoggerInterface;
 
 class AddPhotosToProduct extends \Magenest\InstagramShop\Observer\AddPhotosToProduct implements ObserverInterface
 {
+    /**
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        try {
+            /** @var Product $product */
+            $product = $observer->getEvent()->getProduct();
+            $postData = $this->_request->getPostValue();
+            $photos = [];
+            if (isset($postData['links'][InstagramPhotos::LINK_TYPE])) {
+                $photos = array_column($postData['links'][InstagramPhotos::LINK_TYPE], 'id');
+            }
 
+            $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+            /** @var \Magento\Catalog\Model\Product $orygProduct */
+            $orygProduct = $objectManager->create('Magento\Catalog\Model\Product')->load($product->getId());
+            $instagramPhotosBeforeSave = $orygProduct->getCustomAttribute('instagram_photos')->getValue();
+            $photoIds = $instagramPhotosBeforeSave == '' ? [] : explode(', ', $instagramPhotosBeforeSave);
+            $photosToDelete = array_diff($photoIds, $photos);
+            $this->removeProductFromInstagram($photosToDelete, $product);
+
+            if (count($photos) > 3) {
+                $content = __('You can only add maximum 3 photos per product');
+                ObjectManager::getInstance()->get(ManagerInterface::class)->addNoticeMessage($content);
+                throw new CouldNotSaveException($content);
+            }
+            foreach ($photos as $photo) {
+                $this->addProductToInstagram($photo, $product);
+            }
+            $product->setData(InstagramPhotos::INSTAGRAM_PHOTOS_ATTRIBUTE_CODE, implode(', ', $photos));
+        } catch (\Exception $e) {
+            ObjectManager::getInstance()->get(LoggerInterface::class)->debug('Assign Instagram Photos Exception: ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * @param $photoIds
+     * @param $product
+     * @throws \Exception
+     */
+    public function removeProductFromInstagram($photoIds, $product)
+    {
+        foreach ($photoIds as $id) {
+            $photo = $this->photoFactory->create()->load($id);
+            $productIds = $photo->getProductIds();
+
+            $productIds = $productIds == '' ? [] : explode(', ', $productIds);
+
+            $keyToDelete = null;
+            foreach ($productIds as $key => $value) {
+                if ($value == $product->getId()) {
+                    $keyToDelete = $key;
+                }
+            }
+            unset($productIds[$keyToDelete]);
+
+            $photo->setProductId(implode(', ', $productIds))
+                ->save();
+        }
+    }
 }
