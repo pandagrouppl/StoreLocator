cf2f8cf8dad7fd28232b1479fa72e2bb06879729
Anna Linka <azlinka@light4website.com>
2018-07-31 16:37:08 +0200

    PJ-707 Corrections after call- + - fix, styling, steady search, cat sidebar and view.phtml adjustements

---
+++ app/design/frontend/peterjacksons/petertheme/Smile_ElasticsuiteCatalog/web/requirejs-config.js
@@ -0,0 +1,8 @@
+// Overrides vendor product.html
+// var config = {
+//     map: {
+//         '*': {
+//             "template/autocomplete/product.html" : "Smile_ElasticsuiteCatalog/web/template/autocomplete/product.html"
+//         }
+//     }
+// };
