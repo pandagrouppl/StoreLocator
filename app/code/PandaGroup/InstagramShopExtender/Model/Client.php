c29445e9318de39f5caebdd8a29954edc709d547
mokupniarek <mokupniarek@light4website.com>
2018-10-01 11:42:17 +0200

    [PJ-1000] Fix for dev (change auth url)

--- app/code/PandaGroup/InstagramShopExtender/Model/Client.php
+++ app/code/PandaGroup/InstagramShopExtender/Model/Client.php
@@ -4,5 +4,5 @@ namespace PandaGroup\InstagramShopExtender\Model;
 
 class Client extends \Magenest\InstagramShop\Model\Client
 {
-    protected $redirect_uri_path = 'pj2_back/admin/instagram/instagram/connect/';
+    //protected $redirect_uri_path = 'pj2_back/admin/instagram/instagram/connect/';
 }
